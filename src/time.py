bd30c7787f1b937fdd5ced3b2641edbfc185e891
Scott Haleen <scott.haleen@gmail.com>
2014-04-22 08:30:28 -0400

    fixed utc time issue

--- src/time.py
+++ src/time.py
@@ -10,10 +10,10 @@ def fmtNowISO():
     return datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
 
 def epocTime(long_time):
-    return datetime.datetime.fromtimestamp(long_time)
+    return datetime.datetime.utcfromtimestamp(long_time)
 
 def epochTimeStr(long_time):
-    return datetime.datetime.fromtimestamp(long_time).strftime('%Y-%m-%dT%H:%M:%S')
+    return datetime.datetime.utcfromtimestamp(long_time).strftime('%Y-%m-%dT%H:%M:%S')
 
 def parseStrTimeToEpoch(strTime, pattern):
     return time.mktime(time.strptime(strTime, pattern))
